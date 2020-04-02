d7ad78d221839333a8da6727b418018dcd1b0621
Matthew Desaix <mgdesaix@Matthews-iMac.local>
2020-04-02 08:07:10 -0600

    changed email

--- run-single-job.sh
+++ run-single-job.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=run-single-out-%j
 #SBATCH --error=run-single-error-%j 
 #SBATCH --mail-type=ALL 
-#SBATCH --mail-user=yourname@yourmail.edu
+#SBATCH --mail-user=mgdesaix@gmail.com
 
 
 ./map-N-files-from-K.sh 128 1
